214f438e7d3016bf20fc6f54e17630a05ac7cc4e
David Abell <79927957+david-abell@users.noreply.github.com>
2023-11-01 13:17:49 +0000

    refactor externalImage for srcset sizes

--- src/lib/sanity/imageBuilder.ts
+++ src/lib/sanity/imageBuilder.ts
@@ -1,51 +1,99 @@
 import imageUrlBuilder from '@sanity/image-url';
 import { sanityClient } from 'sanity:client';
 import type { ImageWithExpandedMetaData } from './types';
-import type { Image } from 'sanity';
-
 const builder = imageUrlBuilder(sanityClient);
 
-export type ImageFormFactor =
-  | 'logo'
-  | 'logo-sq'
-  | 'thumb'
-  | 'thumb-lg'
-  | 'social'
-  | 'hero'
-  | 'hero-xs'
-  | 'hero-sq'
-  | 'hero-sm'
-  | 'full';
+export type ImageFormFactor = 'thumb' | 'ogImage' | 'hero' | 'hero-sq' | 'full' | 'portrait';
 
 export const imageSizes = {
-  logo: { width: 250, height: 100 },
-  'logo-sq': { width: 100, height: 100 },
-  thumb: { width: 150, height: 150 },
-  'thumb-lg': { width: 350, height: 350 },
-  social: { width: 32, height: 32 },
-  hero: { width: 1280, height: 720 },
-  'hero-xs': { width: 600, height: 315 },
-  'hero-sm': { width: 1200, height: 630 },
-  'hero-sq': { width: 720, height: 720 },
-  full: { width: 1800, height: 600 },
+  thumb: {
+    small: { width: 150, height: 150 },
+    medium: { width: 250, height: 250 },
+    large: { width: 350, height: 350 },
+  },
+  ogImage: { width: 1200, height: 630 },
+  hero: {
+    small: { width: 600, height: 315 },
+    medium: { width: 1200, height: 630 },
+    large: { width: 1800, height: 945 },
+  },
+  'hero-sq': {
+    small: { width: 315, height: 315 },
+    medium: { width: 500, height: 500 },
+    large: { width: 630, height: 630 },
+  },
+  full: {
+    small: 600,
+    medium: 1200,
+    large: 1800,
+  },
+  portrait: {
+    small: { width: 236, height: 315 },
+    medium: { width: 338, height: 450 },
+    large: { width: 472, height: 630 },
+  },
 } as const;
 
-export function imageBuilder(
-  source: ImageWithExpandedMetaData | Image,
-  size: ImageFormFactor,
-  vertical: boolean = false,
-) {
-  // don't transform svgs
-  if (source.asset?._ref && source.asset?._ref.length >= 3) {
-    if (source.asset?._ref.slice(source.asset?._ref.length - 3).toLowerCase() === 'svg') {
-      return builder.image(source).url();
-    }
-  }
-
-  const { width, height } = imageSizes[size];
+export function imageBuilder(source: ImageWithExpandedMetaData, form: ImageFormFactor) {
   const image = builder.image(source).format('webp');
 
-  return vertical ? image.size(height, width).url() : image.size(width, height).url();
+  if (form === 'ogImage') {
+    return { ogImageUrl: image.size(imageSizes[form].width, imageSizes[form].height).url() };
+  }
+
+  if (form === 'full') {
+    const { aspectRatio } = source.asset.metadata.dimensions;
+    const { small, medium, large } = imageSizes[form];
+
+    if (aspectRatio >= 1) {
+      const smallUrl = image
+        .size(small, Math.round(small / aspectRatio))
+        .crop('center')
+        .fit('crop')
+        .url();
+
+      const mediumUrl = image
+        .size(medium, Math.round(medium / aspectRatio))
+        .crop('center')
+        .fit('crop')
+        .url();
+
+      const largeUrl = image
+        .size(large, Math.round(large / aspectRatio))
+        .crop('center')
+        .fit('crop')
+        .url();
+
+      return { smallUrl, mediumUrl, largeUrl };
+    }
+    const smallUrl = image
+      .size(Math.round(small * aspectRatio), small)
+      .crop('center')
+      .fit('crop')
+      .url();
+
+    const mediumUrl = image
+      .size(Math.round(medium * aspectRatio), medium)
+      .crop('center')
+      .fit('crop')
+      .url();
+
+    const largeUrl = image
+      .size(Math.round(large * aspectRatio), large)
+      .crop('center')
+      .fit('crop')
+      .url();
+
+    return { smallUrl, mediumUrl, largeUrl };
+  }
+
+  const { small, medium, large } = imageSizes[form];
+
+  const smallUrl = image.size(small.width, small.height).url();
+  const mediumUrl = image.size(medium.width, medium.height).url();
+  const largeUrl = image.size(large.width, large.height).url();
+
+  return { smallUrl, mediumUrl, largeUrl };
 }
 
 export function placeholderBuilder(source: ImageWithExpandedMetaData) {
