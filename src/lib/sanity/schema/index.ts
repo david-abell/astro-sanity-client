57249dd73cf2be7387d1227a886ba3b579e21b5b
David Abell <79927957+david-abell@users.noreply.github.com>
2023-11-01 14:59:03 +0000

    add gallery schema

--- src/lib/sanity/schema/index.ts
+++ src/lib/sanity/schema/index.ts
@@ -5,5 +5,6 @@ import post from './post';
 import author from './author';
 import companySettings from './companySettings';
 import homePage from './homePage';
+import gallery from './gallery';
 
-export const schemaTypes = [homePage, post, author, category, companySettings, blockContent, benefit];
+export const schemaTypes = [homePage, post, gallery, author, category, companySettings, blockContent, benefit];
