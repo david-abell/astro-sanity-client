2a6440d03e82592f3834097f976150fb5cce451f
David Abell <79927957+david-abell@users.noreply.github.com>
2024-01-02 12:04:46 +0000

    Styling tweaks (#16)
    * switch contentGrid to single two column row
    
    * fix: Props not assigned a type
    
    * fix: benefits jsx breaks prettier
    
    * update to recommended prettier config
    
    * reduce <p> font size to text-xl
    
    * change problem and solution statements to blocks
    
    * update company introduction to block text
    
    * replace content grid with DoubleFeature
    
    * space-content in Feature
    
    * fix: font sizes
    swap ContentGrid for DoubleFeature
    
    * switch scss container to tailwind container
    
    * add poppins as primary font
    
    * rework hero styling
    
    * update feature accent colors
    
    * add browserslist
    
    * revert cta to primary button color
    
    * add shadow to svg curve
    
    * remove contentMedia outer margins
    
    * fix: consistent margin between section
    
    * fix: content padding
    
    * fix: safari not repainting custom properties
    
    * adjust hero background colors
    
    * move top margin to hero component
    
    * fix: incorrect gallery img sizes
    
    * remove main-content max width
    
    * remove smaller heading resets
    
    * fix: unify spacing
    
    * feat: replace tw container with section component
    
    * Section spacing fixes
    
    * add contentMedia problem/solution
    
    * remove scss container

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
     extend: {},
   },
   plugins: [require('@tailwindcss/typography')],
